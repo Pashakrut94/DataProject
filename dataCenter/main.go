8b6f69ddff02c798e21b2dacc67508dbda9703a9
Pashakrut94 <pashakrut94@gmail.com>
2020-05-16 11:02:46 +0300

    go.mod

---
+++ dataCenter/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	port     = flag.String("port", ":8080", "Local port")
+	pgUser   = flag.String("pg_user", "Pasha", "PostgreSQL name")
+	pgPwd    = flag.String("pg_pwd", "pwd0123456789", "PostgreSQL password")
+	pgHost   = flag.String("pg_host", "localhost", "PostgreSQL host")
+	pgPort   = flag.String("pg_port", "54320", "PostgreSQL port")
+	pgDBname = flag.String("pg_dbname", "mydb", "PostgreSQL name of DB")
+)
+
+func main() {
+	flag.Parse()
+
+	router := mux.NewRouter()
+
+	router.Handle("/region", CreateRegion())
+
+	http.Handle("/", router)
+	fmt.Printf("Server starts at: %s\n", *port)
+	http.ListenAndServe(*port, nil)
+}
+
+func CreateRegion() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Create Region Handler"))
+	}
+}
